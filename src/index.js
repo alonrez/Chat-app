496e5eb1d6eac93ae171d6145dd4836cd0bbf754
alonrez <54244917+alonrez@users.noreply.github.com>
2021-01-17 14:04:56 +0200

    adding socket.io

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const path = require('path')
+const http = require('http') 
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)// Express does it automatically behindthe scenes, but i needed the 'server' variable
+const io = socketio(server) //  Thats why..
 
 const port = process.env.PORT || 3000
 const publcDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publcDirectoryPath))
 
-app.listen(port, () => {
+    io.on('connection', () => {
+        console.log('New Websocket connections')
+    })
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
 
