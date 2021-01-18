85609b6a31785621bbd2510a22acf94c5bec6b6f
alonrez <54244917+alonrez@users.noreply.github.com>
2021-01-18 14:09:21 +0200

    Rendering messages and locations

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ let count = 0
         })
 
         socket.on('sendLocation', (coords, callback) => {
-            io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+            io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
             callback()
         })
 
